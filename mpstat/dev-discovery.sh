3fe3db23bd291dee14bdb1e6cfdbf8c0ba061f66
popexizhi <popexizhi@gmail.com>
2017-06-07 19:06:46 +0800

    add cpu num dev

---
+++ mpstat/dev-discovery.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DEVICES=`mpstat -P ALL | awk '{ if ( $1 ~ "^[0-9]") { print $3} }'|grep -v CPU`
+
+COUNT=`echo "$CPU" | wc -l`
+INDEX=0
+echo '{"data":['
+echo "$DEVICES" | while read LINE; do
+    echo -n '{"{#DEVNAME}":"'$LINE'"}'
+    INDEX=`expr $INDEX + 1`
+    if [ $INDEX -lt $COUNT ]; then
+        echo ','
+    fi
+done
+echo ']}'
+

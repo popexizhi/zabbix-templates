e85d8683f5521b8bac4034d085c34e9ece105ed4
slim <popexizhi@212>
2017-06-08 15:24:26 +0800

    add app_hostid

--- app_noc/app-cron.sh
+++ app_noc/app-cron.sh
@@ -5,12 +5,14 @@ analysis_last_log(){
     local PH_DIR=$1 # log的文件夹位置
     local pre_log="/app_server_[0-9]*.log.txt*" #默认使用log的后缀
     filelist=`find ${PH_DIR} -mtime -1|grep ${pre_log}`
-    datelab="0608/1415"
+    datelab=`date -d "-1 min" +%m%d/%H%M` #过滤前一分钟的数据
+    echo "[`date +%H%M`]${datelab}"
     hostid_tmp="tmp"
+    
     for file in ${filelist}
     do
-        echo "${file}"
-        get_log "${file}" ${datelab} ${hostid_tmp}
+        hostid=`echo "${file}"|sed 's/.*\///g'|sed 's/\(app_server_\)\([0-9]*\).log.txt/\2/g'`
+        get_log "${file}" ${datelab} ${hostid_tmp} ${hostid}
     done
 
 }
@@ -20,6 +22,7 @@ get_log(){
     filepath=$1 #
     datelab=$2 #要过滤的时间段标准
     tmpfile=$3 #存储位置
+    loglab=$4 #来源log标识,当前使用appserver的log名称中的hostid
     local epoll="EpollLoop Throughput statistic" #分钟级别
     local client_count="total online client count" #ue 数量 
     cat ${filepath}|grep "${epoll}"|grep "${datelab}">>${tmpfile}

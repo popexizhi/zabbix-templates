1d64d9879ca38e6d42b879e8d9ae076227febfde
slim <popexizhi@212>
2017-06-08 18:00:26 +0800

    add appserverhost discovery

---
+++ app_noc/appserver-discovery.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+get_app(){
+    #分析最后的更新log使用
+    local PH_DIR=$1 # log的文件夹位置
+    local pre_log="/app_server_[0-9]*.log.txt*" #默认使用log的后缀
+    local hostid_dev=$2
+    filelist=`find ${PH_DIR} -mtime -1|grep ${pre_log}`
+    count=`find ${PH_DIR} -mtime -1|grep ${pre_log}| wc -l`
+    index=0
+
+    echo '{"data":['
+    for file in ${filelist}
+    do
+        hostid=`echo "${file}"|sed 's/.*\///g'|sed 's/\(app_server_\)\([0-9]*\).log.txt/\2/g'`
+        echo -n '{"{#DEVNAME}":"'${hostid}'"}'
+        echo "${hostid}" >>${hostid_dev}
+        (( index+=1 ))
+        if [ $index -lt $count ]; then
+            echo ','
+        fi
+    done
+    echo ']}'
+}
+applist="applist"
+if [ -f ${applist} ]
+then
+    rm ${applist}
+fi
+
+get_app "/home/slim/beta_test/app_server_test" "${applist}" 
+
